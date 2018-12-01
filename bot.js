57a066857a2d19a659dbb687a0185928ab8ab39e
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-01 22:27:25 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,28 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '$ping') {
+    	message.reply('Kebanyakan Nonton Tenis Meja Anda');
+  	}
+    
+    client.on('message', message => {
+    if (message.content === '$makan') {
+    	message.reply('Yang Jelas Makan Nasi Lah');
+  	}
+        
+      client.on('message', message => {
+    if (message.content === '$help') {
+    	message.reply('Prefix:$');
+  	}
+          
+        client.on('message', message => {
+    if (message.content === '$about') {
+    	message.reply('Created By [N0x]RtzFurry');
+  	}
+            
+            client.on('message', message => {
+    if (message.content === '$ghoul') {
+    	message.reply('temannya adli');
   	}
 });
 
