9a14df1e284f5aa041eee5ce01237138dd737b49
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-01 22:38:00 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,28 +8,28 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '$ping') {
     	message.reply('Kebanyakan Nonton Tenis Meja Anda');
-  	}
+});
     
-    client.on('message', message => {
+client.on('message', message => {
     if (message.content === '$makan') {
     	message.reply('Yang Jelas Makan Nasi Lah');
-  	}
+});
         
-      client.on('message', message => {
+client.on('message', message => {
     if (message.content === '$help') {
     	message.reply('Prefix:$');
-  	}
+});
           
-        client.on('message', message => {
+client.on('message', message => {
     if (message.content === '$about') {
     	message.reply('Created By [N0x]RtzFurry');
-  	}
+});
             
-            client.on('message', message => {
+client.on('message', message => {
     if (message.content === '$ghoul') {
     	message.reply('temannya adli');
-  	}
-});
+  	
+});  
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
